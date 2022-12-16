31697c0395f7f273a324baecf4d99ff74d88078b
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2022-12-16 13:35:51 -0500

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ title, description, imgSrc, href }) => (
     <div
       className={`${
         imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      }  overflow-hidden rounded-md border-2 border-gray-700 border-opacity-60 dark:border-gray-300`}
     >
       {imgSrc &&
         (href ? (
@@ -38,11 +38,11 @@ const Card = ({ title, description, imgSrc, href }) => (
             title
           )}
         </h2>
-        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
+        <p className="prose mb-3 max-w-none text-gray-800 dark:text-slate-400">{description}</p>
         {href && (
           <Link
             href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            className="text-base font-medium leading-6 text-indigo-500 hover:text-indigo-600 dark:hover:text-indigo-400"
             aria-label={`Link to ${title}`}
           >
             Learn more &rarr;
