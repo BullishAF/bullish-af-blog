8dd336d1547ed9897e1b6998c8490fd82dc2b4ec
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2022-12-16 13:50:39 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -89,10 +89,7 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter.provider !== '' && (
-        <div className="flex items-center justify-center pt-4">
-        </div>
-      )}
+      
     </>
   )
 }
