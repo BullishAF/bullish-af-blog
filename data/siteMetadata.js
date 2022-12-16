c7e8d9098808b6d732e7d585c8f82f24fe2f5695
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2022-12-16 11:16:36 -0500

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'BullishAF Blog',
   author: 'Bullish AF',
   headerTitle: 'BullishAF Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Thoughts on crypto, defi, nfts and other random things',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://bullish-af-blog.vercel.app/',
@@ -11,11 +11,8 @@ const siteMetadata = {
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  github: 'https://github.com/BullishAF',
+  twitter: 'https://twitter.com/RP_BullishAF',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
